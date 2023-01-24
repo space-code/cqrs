54f5c1b0ea0478a5b5b76c574b9aaf8f4b2c467a
Nikita Vasilev <nv3212@gmail.com>
2023-01-24 17:44:52 +0400

    Generate `CQRS` package

---
+++ Tests/CQRSTests/CQRSTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CQRS
+
+final class CQRSTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CQRS().text, "Hello, World!")
+    }
+}
